f1ce39dcc2d4b4824d5907c6064773f8348c4988
liao <2735398076@qq.com>
2017-07-05 17:41:07 +0800

    start widget

---
+++ src/AutherWidget.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 27353 on 2017/7/5.
+ */
+define(["dojo/_base/declare","dijit/_WidgetBase","dijit/_TemplateMixin"],
+    function (declare,_WidgetBase,_TemplateMixin) {
+        return declare([_WidgetBase,_TemplateMixin],{
+
+        });
+    })
