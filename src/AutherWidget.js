06ca91b752987053e320ae5c880aecb5b523beab
liao <2735398076@qq.com>
2017-07-06 09:26:25 +0800

    widget

--- src/AutherWidget.js
+++ src/AutherWidget.js
@@ -1,9 +1,69 @@
 /**
  * Created by 27353 on 2017/7/5.
  */
-define(["dojo/_base/declare","dijit/_WidgetBase","dijit/_TemplateMixin"],
-    function (declare,_WidgetBase,_TemplateMixin) {
-        return declare([_WidgetBase,_TemplateMixin],{
+define(["dojo/_base/declare",
+        "dojo/_base/fx",
+        "dojo/_base/lang",
+        "dojo/dom-style",
+        "dojo/mouse",
+        "dojo/on",
+        "dijit/_WidgetBase",
+        "dijit/_TemplateMixin"],
+    function (declare, baseFx, lang, domStyle, mouse, on , _WidgetBase,_TemplateMixin,template) {
+    return declare([_WidgetBase,_TemplateMixin],{
+
+            name:"No Name",
+
+            avator: require.toUrl("./widget/images/mfc.PNG"),
+
+            bio:"",
+
+            templateString: template,
+
+            baseClass:"autherWidget",
+
+            mouseAnim: null,
+
+            baseBackgroundColor:"#fff",
+            mouseBackgroundColor:"#def",
+
+            postCreate:function () {
+                var domNode = this.domNode;
+
+                this.inherited(arguments);
+
+                domStyle.set(domNode, "backgroundColor",this.baseBackgroundColor);
+
+                this.own(
+                    on(domNode, mouse.enter ,lang.hitch(this, "_changeBackground",this.mouseBackgroundColor)),
+                    on(domNode, mouse.leave ,lang.hitch(this, "_changeBackground", this.baseBackgroundColor))
+                )
+            },
+
+            _changeBackground:function (newColor) {
+                if(this.mouseAnim){
+                    this.mouseAnim.stop();
+                }
+
+                this.mouseAnim = baseFx.animateProperty({
+                    node:this.domNode,
+                    properties:{
+                        backgroundColor: newColor
+                    },
+                    onEnd: lang.hitch(this,function () {
+                        this.mouseAnim = null;
+                    })
+                }).play();
+            },
+
+            _setAvatorAttr:function (imagePath) {
+
+                if(imagePath != ""){
+                    this._set("avatar",imagePath);
+
+                    this.avatarNode.src = imagePath;  //link to html
+                }
+            }
 
         });
-    })
+    });
